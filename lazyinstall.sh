80643d5b0961096ef829133ec517af0f8679e3eb
Marc-Antoine Bouchard Marceau <marc-antoine.bm@libeo.com>
2016-08-03 20:10:43 -0400

    Lazy setup

---
+++ lazyinstall.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# base source
+git clone git@github.com:bouchardm/conge-app-laravel.git
+cd conge-app-laravel
+
+# external source
+composer install
+npm install # sudo chown -R $(whoami) ~/.npm
+gulp
+
+# .env file
+cp .env.example .env
+php artisan key:generate
+
+# lazy database
+touch database/database.sqlite
+php artisan migrate
+
+# ide-helper
+php artisan clear-compiled
+php artisan ide-helper:generate
