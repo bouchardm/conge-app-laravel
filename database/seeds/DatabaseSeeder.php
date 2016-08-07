34ca82f6fa3152812f0bc01a98dd4aaadc7deaf1
Marc-Antoine Bouchard Marceau <marc-antoine.bm@libeo.com>
2016-08-06 22:42:10 -0400

    User seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(DemandeSeeder::class);
+        $this->call(DemandeSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
