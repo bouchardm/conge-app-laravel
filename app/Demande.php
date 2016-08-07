9a62e034b85198515dd4add1c15ab1c37df6c882
Marc-Antoine Bouchard Marceau <marc-antoine.bm@libeo.com>
2016-08-06 22:32:34 -0400

    Demande -> eloquent

--- app/Demande.php
+++ app/Demande.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Demande extends Model
+class Demande extends \Eloquent
 {
     protected $fillable = ['raison', 'debut', 'fin', 'type'];
 }
